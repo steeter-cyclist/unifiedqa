b99fcd9f85441a27e41bcc82ef4e31dba6937851
Sam Teeter <samteeter93@yahoo.com>
2021-06-07 16:27:00 -0500

    Fix indent error

--- cycic_task.py
+++ cycic_task.py
@@ -57,22 +57,22 @@ def dataset_fn(split, shuffle_files=False, dataset=""):
 
 def load_cycic():
     for dataset in DATASETS:
-    print(f" >>>> reading dataset: {dataset}")
-    t5.data.set_tfds_data_dir_override(DATA_DIR + dataset)
-    t5.data.TaskRegistry.add(
-        f"{dataset}_task",
-        # Supply a function which returns a tf.data.Dataset.
-        dataset_fn=functools.partial(dataset_fn, dataset=dataset),
-        splits=["train", "dev", "test"],
-        # Supply a function which preprocesses text from the tf.data.Dataset.
-        text_preprocessor=[dataset_preprocessor],
-        # Use the same vocabulary that we used for pre-training.
-        sentencepiece_model_path=t5.data.DEFAULT_SPM_PATH,
-        # Lowercase targets before computing metrics.
-        postprocess_fn=t5.data.postprocessors.lower_text,
-        metric_fns=[t5.evaluation.metrics.accuracy],
-    )
-    print(f" >>>> adding one mixture per dataset: `{dataset}_mixture`")
-    t5.data.MixtureRegistry.add(
-        f"{dataset}_mixture", [f"{dataset}_task"], default_rate=1.0
-    )
+        print(f" >>>> reading dataset: {dataset}")
+        t5.data.set_tfds_data_dir_override(DATA_DIR + dataset)
+        t5.data.TaskRegistry.add(
+            f"{dataset}_task",
+            # Supply a function which returns a tf.data.Dataset.
+            dataset_fn=functools.partial(dataset_fn, dataset=dataset),
+            splits=["train", "dev", "test"],
+            # Supply a function which preprocesses text from the tf.data.Dataset.
+            text_preprocessor=[dataset_preprocessor],
+            # Use the same vocabulary that we used for pre-training.
+            sentencepiece_model_path=t5.data.DEFAULT_SPM_PATH,
+            # Lowercase targets before computing metrics.
+            postprocess_fn=t5.data.postprocessors.lower_text,
+            metric_fns=[t5.evaluation.metrics.accuracy],
+        )
+        print(f" >>>> adding one mixture per dataset: `{dataset}_mixture`")
+        t5.data.MixtureRegistry.add(
+            f"{dataset}_mixture", [f"{dataset}_task"], default_rate=1.0
+        )
